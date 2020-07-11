f3d5c13664e1bd5ebe77c8bf9b8de6e056f6c585
jullian-yapeter <jullianyapeter@gmail.com>
2020-07-11 19:02:26 -0400

    restructured data augmentation settings input format (dict) and added resize function

--- dataNodes/dataAugmentationNode.py
+++ dataNodes/dataAugmentationNode.py
@@ -14,9 +14,9 @@ class DatasetAugmentation():
         try:
             sm = SettingsManager()
             self.dataAugmentationSettings = sm.readField("dataAugmentationNode")
-            logs.debugging.info("data augmentation settings loaded successfully")
+            logs.dataAugmentation.info("data augmentation settings loaded successfully")
         except Exception as e:
-            logs.debugging.error("Error while opening settings file, %s", e)
+            logs.dataAugmentation.error("Error while opening settings file, %s", e)
         self.transforms = self.composeTransform()
 
     def composeTransform(self):
@@ -24,21 +24,33 @@ class DatasetAugmentation():
         builds a PyTorch transform based on the passed in settings
         :return transforms [dict] : dictionary of transforms (for training and test sets)
         """
-        transforms = {'train': tt.ToTensor(), 'test': tt.ToTensor()}  # default
-        trainTransforms = []
-        testTransforms = []
-        # construct training transforms
-        if "hflip" in self.dataAugmentationSettings["train"]:
-            trainTransforms.append(tt.RandomHorizontalFlip())
-        if "vflip" in self.dataAugmentationSettings["train"]:
-            trainTransforms.append(tt.RandomVerticalFlip())
-        trainTransforms.append(tt.ToTensor())
-        transforms["train"] = tt.Compose(trainTransforms)
-        # construct testing transforms
-        if "hflip" in self.dataAugmentationSettings["test"]:
-            testTransforms.append(tt.RandomHorizontalFlip())
-        if "vflip" in self.dataAugmentationSettings["test"]:
-            testTransforms.append(tt.RandomVerticalFlip())
-        testTransforms.append(tt.ToTensor())
-        transforms["test"] = tt.Compose(testTransforms)
-        return transforms
+        try:
+            transforms = {'train': tt.ToTensor(), 'test': tt.ToTensor()}  # default
+            trainTransforms = []
+            testTransforms = []
+            # construct training transforms
+            trainSettings = self.dataAugmentationSettings["train"]
+            trainAugmentationSelections = trainSettings.keys()
+            if "resize" in trainAugmentationSelections:
+                trainTransforms.append(tt.Resize(trainSettings.get("resize")))
+            if "hflip" in trainAugmentationSelections:
+                trainTransforms.append(tt.RandomHorizontalFlip(trainSettings.get("hflip")))
+            if "vflip" in trainAugmentationSelections:
+                trainTransforms.append(tt.RandomVerticalFlip(trainSettings.get("vflip")))
+            trainTransforms.append(tt.ToTensor())
+            transforms["train"] = tt.Compose(trainTransforms)
+            # construct testing transforms
+            testSettings = self.dataAugmentationSettings["test"]
+            testAugmentationSelections = testSettings.keys()
+            if "resize" in testAugmentationSelections:
+                testTransforms.append(tt.Resize(testSettings.get("resize")))
+            if "hflip" in testAugmentationSelections:
+                testTransforms.append(tt.RandomHorizontalFlip(testSettings.get("hflip")))
+            if "vflip" in testAugmentationSelections:
+                testTransforms.append(tt.RandomVerticalFlip(testSettings.get("vflip")))
+            testTransforms.append(tt.ToTensor())
+            transforms["test"] = tt.Compose(testTransforms)
+            logs.dataAugmentation.info("data augmentation transforms composed successfully")
+            return transforms
+        except Exception as e:
+            logs.dataAugmentation.error("Error data augmentation transforms composition failed, %s", e)
